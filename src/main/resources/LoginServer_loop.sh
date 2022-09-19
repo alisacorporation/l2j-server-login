42ca00bcd47456a8ae486ba7e2942b3852de5aa8
alisa corp <amg.lv@protonmail.com>
2022-09-20 02:48:32 +0300

    first commit

---
+++ src/main/resources/LoginServer_loop.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# exit codes of LoginServer:
+#  0 normal shutdown
+#  2 reboot attempt
+
+while :; do
+	[ -f log/java0.log.0 ] && mv log/java0.log.0 "log/`date +%Y-%m-%d_%H-%M-%S`_java.log"
+	[ -f log/stdout.log ] && mv log/stdout.log "log/`date +%Y-%m-%d_%H-%M-%S`_stdout.log"
+	java -Xms128m -Xmx256m -jar l2jlogin.jar > log/stdout.log 2>&1
+	[ $? -ne 2 ] && break
+	sleep 10
+done
